54c0ce7181a7a4b8bbb68f668468e819d0c51434
Si-An <d09922007@csie.ntu.edu.tw>
2021-01-21 19:35:39 +0800

    add last step to results

--- src/collect_result.py
+++ src/collect_result.py
@@ -16,7 +16,7 @@ def parse_args():
 def get_best_fid_from_log(log_path):
     with open(log_path) as fp:
         lines = fp.readlines()
-    step, best_fid, f8, finv8, IS = -999, 999, -1, -1, -1
+    step, best_fid, f8, finv8, IS, last_step = -999, 999, -1, -1, -1, -1
     for line in lines[::-1]:
         if 'Best FID score' in line:
             step = int(re.search('Step: (?P<step>\d+)', line)['step'])
@@ -29,26 +29,29 @@ def get_best_fid_from_log(log_path):
             finv8 = float(re.search('([0-9\.]+)$', line).group())
         if str(step) in line and 'Inception score' in line:
             IS = float(re.search('([0-9\.]+)$', line).group())
-    return step, best_fid, IS, f8, finv8
+        if 'Step: ' in line and 'FID' in line:
+            last_step = max(last_step, int(re.search('Step: (?P<step>\d+)', line)['step']))
+    return step, best_fid, IS, f8, finv8, last_step
 
 
 def main():
     args = parse_args()
     log_dir = args.log_dir
-    metrics = ['step', 'best_fid', 'IS', 'f_8', 'f_1/8']
+    cols = ['step', 'best_fid', 'IS', 'f_8', 'f_1/8', 'last_step']
     exp_names, best_fids, steps = [], [], []
     records = []
     for log_path in glob.glob(os.path.join(log_dir, '*.log')):
         exp_name = log_path.split('/')[-1]
         exp_name = exp_name[:exp_name.find('-train-')]
-        record = dict(zip(metrics, get_best_fid_from_log(log_path)))
+        record = dict(zip(cols, get_best_fid_from_log(log_path)))
         record['exp_name'] = exp_name
         records.append(record)
-    df = pd.DataFrame.from_records(records, columns=['exp_name'] + metrics)
+    df = pd.DataFrame.from_records(records, columns=['exp_name'] + cols)
     df = df.sort_values('best_fid')
+    print(df)
     avg_df = df.groupby('exp_name').agg(['mean', 'std'])
-    print(avg_df)
     avg_df = avg_df.sort_values(('best_fid', 'mean'))
+    print(avg_df)
     with pd.ExcelWriter('result.xlsx') as writer:
         df.to_excel(writer, sheet_name='full_result')
         avg_df.to_excel(writer, sheet_name='avg_result')
