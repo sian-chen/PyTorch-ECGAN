2629a3ce9886bf15503901b5b65bc14d0a3e235b
Si-An <d09922007@csie.ntu.edu.tw>
2021-01-22 10:36:16 +0800

    update tinyimagenet exp order

--- src/scripts/TINY_ILSVRC2012/exp1_3.sh
+++ src/scripts/TINY_ILSVRC2012/exp1_3.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 CUDA_VISIBLE_DEVICES=0,1,2,3 python src/main.py -t -e --eval_type valid --seed 0 -c src/exp_configs/TINY_ILSVRC2012/ecgan2_contragan_ce1.json &
-CUDA_VISIBLE_DEVICES=4,5,6,7 python src/main.py -t -e --eval_type valid --seed 0 -c src/exp_configs/TINY_ILSVRC2012/ecgan2_ntxent_ce1.json    &
+CUDA_VISIBLE_DEVICES=4,5,6,7 python src/main.py -t -e --eval_type valid --seed 0 -c src/exp_configs/TINY_ILSVRC2012/ecgan2_none_ce1.json &
 wait
 CUDA_VISIBLE_DEVICES=0,1,2,3 python src/main.py -t -e --eval_type valid --seed 1 -c src/exp_configs/TINY_ILSVRC2012/ecgan2_contragan_ce1.json &
-CUDA_VISIBLE_DEVICES=4,5,6,7 python src/main.py -t -e --eval_type valid --seed 1 -c src/exp_configs/TINY_ILSVRC2012/ecgan2_ntxent_ce1.json    &
+CUDA_VISIBLE_DEVICES=4,5,6,7 python src/main.py -t -e --eval_type valid --seed 1 -c src/exp_configs/TINY_ILSVRC2012/ecgan2_none_ce1.json &
