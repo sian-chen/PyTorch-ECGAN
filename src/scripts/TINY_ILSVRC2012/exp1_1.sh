860fd320d8f6426ac01bce4cc1d69a9b13230757
Si-An <d09922007@csie.ntu.edu.tw>
2021-01-23 09:40:21 +0800

    update tiny imagenet scripts

--- src/scripts/TINY_ILSVRC2012/exp1_1.sh
+++ src/scripts/TINY_ILSVRC2012/exp1_1.sh
@@ -1,6 +1,5 @@
 #!/bin/bash
-CUDA_VISIBLE_DEVICES=0,1,2,3 python src/main.py -t -e --eval_type valid --seed 0 -c src/exp_configs/TINY_ILSVRC2012/ecgan1_none.json      &
-CUDA_VISIBLE_DEVICES=4,5,6,7 python src/main.py -t -e --eval_type valid --seed 0 -c src/exp_configs/TINY_ILSVRC2012/ecgan1_contragan.json &
-wait
-CUDA_VISIBLE_DEVICES=0,1,2,3 python src/main.py -t -e --eval_type valid --seed 1 -c src/exp_configs/TINY_ILSVRC2012/ecgan1_none.json      &
-CUDA_VISIBLE_DEVICES=4,5,6,7 python src/main.py -t -e --eval_type valid --seed 1 -c src/exp_configs/TINY_ILSVRC2012/ecgan1_contragan.json &
+for i in {0..2};
+do
+  CUDA_VISIBLE_DEVICES=0,1,2,3,4,5,6,7 python src/main.py -t -e --eval_type valid -c src/configs/TINY_ILSVRC2012/ecgan1_none.json --seed $i
+done
--- src/scripts/TINY_ILSVRC2012/exp1_7.sh
+++ src/scripts/TINY_ILSVRC2012/exp1_7.sh
@@ -1,6 +1,5 @@
 #!/bin/bash
-CUDA_VISIBLE_DEVICES=0,1,2,3 python src/main.py -t -e --eval_type valid --seed 2 -c src/exp_configs/TINY_ILSVRC2012/ecgan1_none.json      &
-CUDA_VISIBLE_DEVICES=4,5,6,7 python src/main.py -t -e --eval_type valid --seed 2 -c src/exp_configs/TINY_ILSVRC2012/ecgan1_contragan.json &
-wait
-CUDA_VISIBLE_DEVICES=0,1,2,3 python src/main.py -t -e --eval_type valid --seed 2 -c src/exp_configs/TINY_ILSVRC2012/ecgan1_ntxent.json    &
-CUDA_VISIBLE_DEVICES=4,5,6,7 python src/main.py -t -e --eval_type valid --seed 2 -c src/exp_configs/TINY_ILSVRC2012/ecgan2_none_ce1.json  &
+for i in {0..2};
+do
+  CUDA_VISIBLE_DEVICES=0,1,2,3,4,5,6,7 python src/main.py -t -e --eval_type valid -c src/configs/TINY_ILSVRC2012/ecgan2_none_ce0p01.json --seed $i
+done
