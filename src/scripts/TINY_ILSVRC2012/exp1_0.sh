9cfb57763fb1668e72456a45d8890e79ac6394a2
Si-An <d09922007@csie.ntu.edu.tw>
2021-01-23 08:03:45 +0800

    fix tiny imagenet baseline script

--- src/scripts/TINY_ILSVRC2012/exp1_0.sh
+++ src/scripts/TINY_ILSVRC2012/exp1_0.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 for i in {0..2};
 do
-  CUDA_VISIBLE_DEVICES=0,1,2,3 python src/main.py -t -e -c src/configs/TINY_ILSVRC2012/BigGAN.json --seed $i &
-  CUDA_VISIBLE_DEVICES=4,5,6,7 python src/main.py -t -e -c src/configs/TINY_ILSVRC2012/ContraGAN.json --seed $i &
+  CUDA_VISIBLE_DEVICES=0,1,2,3 python src/main.py -t -e --eval_type valid -c src/configs/TINY_ILSVRC2012/BigGAN.json --seed $i &
+  CUDA_VISIBLE_DEVICES=4,5,6,7 python src/main.py -t -e --eval_type valid -c src/configs/TINY_ILSVRC2012/ContraGAN.json --seed $i &
   wait
 done
