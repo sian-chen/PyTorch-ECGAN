d37b395ecbee27a4ac9ba15d3a2edc4cf5287221
Si-An <d09922007@csie.ntu.edu.tw>
2021-01-27 22:17:31 +0800

    tune tiny imagenet ecgan lr

---
+++ src/scripts/tune_tiny/tune1.sh
@@ -0,0 +1,5 @@
+#!/bin/bash
+CUDA_VISIBLE_DEVICES=0,1 python src/main.py -t -e --eval_type valid -c src/exp_configs/tune_tiny/ecgan1_none_lr8e-04_2e-04.json --seed 0
+CUDA_VISIBLE_DEVICES=2,3 python src/main.py -t -e --eval_type valid -c src/exp_configs/tune_tiny/ecgan1_none_lr4e-04_2e-04.json --seed 0
+CUDA_VISIBLE_DEVICES=4,5 python src/main.py -t -e --eval_type valid -c src/exp_configs/tune_tiny/ecgan1_none_lr2e-04_2e-04.json --seed 0
+CUDA_VISIBLE_DEVICES=6,7 python src/main.py -t -e --eval_type valid -c src/exp_configs/tune_tiny/ecgan1_none_lr1e-04_2e-04.json --seed 0
