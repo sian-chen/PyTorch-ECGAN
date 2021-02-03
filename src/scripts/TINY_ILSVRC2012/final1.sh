6b9808865171638f2b00f90f2e8df5eb8276fbc5
Si-An <d09922007@csie.ntu.edu.tw>
2021-02-03 12:23:19 +0800

    update script

--- src/scripts/TINY_ILSVRC2012/final1.sh
+++ src/scripts/TINY_ILSVRC2012/final1.sh
@@ -1,7 +1,5 @@
 #!/bin/bash
-CUDA_VISIBLE_DEVICES=0,1 python src/main.py -t -e --eval_type valid -c src/exp_configs/tune_tiny/ecgan_0p9_0p1_none_ce0p01.json --seed 1 &
+CUDA_VISIBLE_DEVICES=0,1 python src/main.py -t -e --eval_type valid -c src/exp_configs/tune_tiny/ecgan_0p9_0p1_none_ce0p01.json --seed 2 &
 sleep 180
-CUDA_VISIBLE_DEVICES=2,3 python src/main.py -t -e --eval_type valid -c src/exp_configs/tune_tiny/ecgan_0p9_0p1_none_ce0p01.json --seed 2 &
-sleep 180
-CUDA_VISIBLE_DEVICES=4,5 python src/main.py -t -e --eval_type valid -c src/exp_configs/tune_tiny/ecgan_0p9_0p1_none_ce0p01.json --seed 3 &
+CUDA_VISIBLE_DEVICES=2,3 python src/main.py -t -e --eval_type valid -c src/exp_configs/tune_tiny/ecgan_0p9_0p1_none_ce0p01.json --seed 3 &
 wait
